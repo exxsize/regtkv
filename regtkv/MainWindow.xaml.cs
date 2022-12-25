f79d7e19ce89b68dbbce94cbd8bcae0bcc2b464d
Кирилл Томилов <tomilovk@list.ru>
2022-12-25 15:07:52 +0300

    Добавьте файлы проекта.

---
+++ regtkv/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace regtkv
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //Обработка нажатия кнопки зарегистрироваться
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+
+            //Создание переменных
+            string login = textBoxLogin.Text.Trim();
+            string pass = passBox.Password.Trim();
+            string pass_2 = passBox_2.Password.Trim();
+            string email = textBoxEmail.Text.Trim().ToLower();
+
+            //Проверка длины логина
+            if(login.Length < 5) 
+            {
+                textBoxLogin.ToolTip = "Это поле введено не корректно!";
+                textBoxLogin.Background = Brushes.DarkRed;
+            }
+
+            //Проверка длины пароля
+            else if (pass.Length < 5)
+            {
+                passBox.ToolTip = "Это поле введено не корректно!";
+                passBox.Background = Brushes.DarkRed;
+            }
+
+            //Проверка совпадения пароля
+            else if (pass != pass_2)
+            {
+                passBox_2.ToolTip = "Это поле введено не корректно!";
+                passBox_2.Background = Brushes.DarkRed;
+            }
+
+            //Проверка почты
+            else if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                textBoxEmail.ToolTip = "Это поле введено не корректно!";
+                textBoxEmail.Background = Brushes.DarkRed;
+            }
+
+            //Если всё хорошо то всё хорошо
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                passBox.ToolTip = "";
+                passBox.Background = Brushes.Transparent;
+                passBox_2.ToolTip = "";
+                passBox_2.Background = Brushes.Transparent;
+                textBoxEmail.ToolTip = "";
+                textBoxEmail.Background = Brushes.Transparent;
+
+                MessageBox.Show("Всё хорошо!");
+            }
+        }
+    }
+}
---
+++ regtkv/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace regtkv
+{
+    internal class User
+    {
+
+        private int id { get; set; }
+
+        private string login, email, pass;
+
+        public User() { }
+
+        public User(string login, string email, string pass)
+        {
+            this.login = login;
+            this.email = email;
+            this.pass = pass;
+        }
+                
+    }
+}
